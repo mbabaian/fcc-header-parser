91b2150f357f8313c0902a27abdb7566a77a7050
Monica <monica.babaian@gmail.com>
2018-02-05 18:07:26 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+// basic required imports for NodeJS
+var express = require('express')
+var bodyParser = require('body-parser')
+var cors = require('cors')
+// easy parsing of user-agent for response
+var useragent = require('express-useragent')
+// create instance of app
+var app = module.exports = express()
+
+app.use(bodyParser.json())
+app.use(cors())
+app.use(useragent.express())
+
+// API url 
+var api = '/api/whoami'
+
+app.get(api, function(req, res, next) {
+    var language = req.acceptsLanguages()
+    var software = "os: " + req.useragent.os + ", Browser: " + req.useragent.browser
+    var ipAddress = req.ip
+
+    res.json({'ipaddress': ipAddress, 'language': language[0], 'software': software})
+    //console.log('App function is working, too!')
+});
+
+app.listen(3000, function() {
+    console.log('App is working!')
+});
